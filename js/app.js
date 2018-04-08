0ee92a265c13c6e7478d61931d9d21de47e2e168
Anas <anas2006101@gmail.com>
2018-04-08 14:57:40 +0300

    randomizing the cards

--- js/app.js
+++ js/app.js
@@ -2,17 +2,43 @@
  * Create a list that holds all of your cards
  */
 
-
+var icons = [
+    'fa-diamond',
+    'fa-paper-plane-o',
+    'fa-anchor',
+    'fa-bolt',
+    'fa-cube',
+    'fa-leaf',
+    'fa-bicycle',
+    'fa-bomb',
+    'fa-diamond',
+    'fa-paper-plane-o',
+    'fa-anchor',
+    'fa-bolt',
+    'fa-cube',
+    'fa-leaf',
+    'fa-bicycle',
+    'fa-bomb'
+];
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+var shuffled = icons;
+$(document).ready(function () {
+    var shuffled = shuffle(icons);
+    show(shuffled);
+    $(".card").click(function () {
+        $(this).toggleClass("open show");
+    })
+});
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -23,6 +49,12 @@ function shuffle(array) {
     }
 
     return array;
+}
+
+function show(array){
+    array.forEach(function(item){
+    $('.deck').append('<li class="card"><i class="fa '+item+'"></i></li>');
+})
 }
 
 
@@ -36,3 +68,15 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+
+
+function checkOpened() {
+
+}
+
+
+function checkMatched(element) {
+
+}
